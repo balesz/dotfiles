5174d8c2f298ecef4b7336239bfcff5d24526563
Bálint Böde <balint.bode@gmail.com>
2022-04-25 22:49:12 +0200

    Add .config/nvim

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,16 @@
+
+-- using the mouse
+vim.cmd[[
+	set mouse=a
+]]
+
+-- tabs and indenting
+vim.cmd [[
+	set tabstop=2
+	set shiftwidth=2
+	set smarttab
+	set autoindent
+	set smartindent
+	set expandtab
+]]
+
