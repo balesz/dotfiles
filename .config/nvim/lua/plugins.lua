3bf63f341325fefc088c4ea52b3ced5c8c41a8de
Bálint Böde <balint.bode@gmail.com>
2022-04-27 23:45:40 +0200

    Add setup/setup.lua file to nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,14 +6,35 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
+  -- packer
   use 'wbthomason/packer.nvim'
- 
-  use 'kyazdani42/nvim-web-devicons' 
-  use "EdenEast/nightfox.nvim"
-  use 'nvim-lualine/lualine.nvim'
-end)
 
-require('lualine').setup {
-  options = { theme = 'gruvbox' }
-}
+  -- common
+  use 'nvim-lua/plenary.nvim'
+
+  -- user interface
+  use 'karb94/neoscroll.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'EdenEast/nightfox.nvim'
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  -- terminal
+  use 'voldikss/vim-floaterm'
+
+  -- editor
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- snippet
+  use 'L3MON4D3/LuaSnip'
+
+  -- lsp server / autocompletion
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'saadparwaiz1/cmp_luasnip'
+end)
 
---
+++ .config/nvim/lua/setup/setup.lua
@@ -0,0 +1,13 @@
+require("indent_blankline").setup {
+}
+
+require('lualine').setup {
+  options = { theme = 'gruvbox' }
+}
+
+require('neoscroll').setup {
+}
+
+require('telescope').setup {
+}
+
