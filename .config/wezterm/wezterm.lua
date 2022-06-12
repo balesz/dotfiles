42ac59877521ad3528e4f69aa03ea7dba244db6d
Balesz <balint.bode@gmail.com>
2022-06-12 21:54:30 +0200

    Change windows specific settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,25 @@
 local wezterm = require 'wezterm';
 
 local default_prog = nil
+local font = 'JetBrainsMono Nerd Font'
+
 if os.getenv('OS') == 'Windows_NT' then
-  default_prog = { 'wsl', '-d', 'Ubuntu' }
+  default_prog = { 'wsl', '-d', 'Ubuntu', '--cd', '~' }
+  font = 'JetBrainsMono NF'
 end
 
 return {
-  font = wezterm.font('JetBrainsMono Nerd Font'),
+  default_prog = default_prog,
+  font = wezterm.font(font),
   font_size = 11,
   initial_cols = 120,
   initial_rows = 40,
-  default_prog = default_prog,
+  window_background_opacity = 0.9,
+  window_padding = {
+    left = '0cell',
+    right = '0cell',
+    top = '0cell',
+    bottom = '0cell',
+  }
 }
+
