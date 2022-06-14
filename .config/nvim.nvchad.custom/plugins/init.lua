f9d8ca14b49735d2fd952ada42db99754c33a161
Bálint Böde <balint.bode@gmail.com>
2022-06-14 21:23:36 +0200

    Add rest-nvim plugin

--- .config/nvim.nvchad.custom/plugins/init.lua
+++ .config/nvim.nvchad.custom/plugins/init.lua
@@ -14,6 +14,13 @@ return {
     end
   },
 
+  ['NTBBloodbath/rest.nvim'] = {
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      require('rest-nvim').setup(require 'custom.plugins.rest-nvim')
+    end
+  },
+
   ['dart-lang/dart-vim-plugin'] = { disable = not disableFlutterTools },
   ['thosakwe/vim-flutter'] = { disable = not disableFlutterTools },
   ['akinsho/flutter-tools.nvim'] = {
