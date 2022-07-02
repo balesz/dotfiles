17473cbd77ba48250a866ae0ded1f5edcc81fddb
Bálint Böde <balint.bode@gmail.com>
2022-07-02 20:14:24 +0200

    Change init.lua

--- .config/nvim.nvchad.custom/init.lua
+++ .config/nvim.nvchad.custom/init.lua
@@ -1,9 +1,14 @@
-local set = vim.opt
+local set = vim.o
+local global = vim.g
+local autocmd = vim.api.nvim_create_autocmd
 
 local font = 'JetBrainsMono Nerd Font:h11'
 if (os.getenv 'WSL_DISTRO_NAME') then
   font = 'JetBrainsMono NF:h11'
 end
+
+vim.opt.syntax = 'on'
+vim.o.syntax = 'on'
 
 -- ui
 set.guifont = font
@@ -11,26 +16,32 @@ set.colorcolumn = 120
 
 -- indent
 set.tabstop = 2
---set.expandtab = true
+set.expandtab = true
 set.shiftwidth = 2
 set.autoindent = true
 set.smartindent = true
 
 -- dart
-vim.g['dart_style_guide'] = 2
-vim.g['dart_format_on_save'] = 1
-vim.g['dartfmt_options'] = {'-l 120'}
+global.dart_style_guide = 2
+global.dart_format_on_save = 1
+global.dartfmt_options = {'-l 120'}
 
 -- flutter
-vim.g['flutter_hot_reload_on_save'] = 1
-vim.g['flutter_show_log_on_run'] = 'hidden'
-vim.g['flutter_show_log_on_attach'] = 'hidden'
-vim.g['flutter_autoscroll'] = 1
-vim.g['flutter_use_last_run_option'] = 1
-vim.g['flutter_use_last_attach_option'] = 1
+global.flutter_hot_reload_on_save = 1
+global.flutter_show_log_on_run = 'hidden'
+global.flutter_show_log_on_attach = 'hidden'
+global.flutter_autoscroll = 1
+global.flutter_use_last_run_option = 1
+global.flutter_use_last_attach_option = 1
 
 -- terminal
-vim.cmd [[
-  autocmd TermOpen * setlocal nonumber norelativenumber
-]]
+autocmd('TermOpen', { pattern = '*', callback = function ()
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+end })
+
+-- Makefile
+autocmd('FileType', { pattern = 'make', callback = function ()
+  vim.bo.expandtab = false
+end })
 
