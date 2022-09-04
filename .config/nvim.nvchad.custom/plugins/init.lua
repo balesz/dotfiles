49891e453e013b68cf3adc33f6942c39c3eac15e
Bálint Böde <balint.bode@gmail.com>
2022-09-04 12:16:00 +0200

    Disable colorizer

--- .config/nvim.nvchad.custom/plugins/init.lua
+++ .config/nvim.nvchad.custom/plugins/init.lua
@@ -1,19 +1,8 @@
 return {
 
-  ["sunjon/shade.nvim"] = {
-    disable = false,
-    config = function ()
-      require'shade'.setup({
-        overlay_opacity = 50,
-        opacity_step = 1,
-        keys = {
-          brightness_up    = '<C-Up>',
-          brightness_down  = '<C-Down>',
-          toggle           = '<Leader>s',
-        }
-      })
-    end
-   },
+  ["NvChad/nvim-colorizer.lua"] = {
+    disable = true,
+  },
 
   ["MunifTanjim/exrc.nvim"] = {
     config = function ()
