3ea5c0f6020b071bacce333793b79cd5b6732857
Bálint Böde <balint.bode@gmail.com>
2022-09-04 16:52:18 +0200

    Change dap config

--- .config/nvim.nvchad.custom/plugins/dapconfig.lua
+++ .config/nvim.nvchad.custom/plugins/dapconfig.lua
@@ -1,4 +1,16 @@
-require("dap").adapters.dart = {
+local dap, dapui = require("dap"), require("dapui")
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+dap.adapters.dart = {
   type = "executable",
   command = "flutter",
   args = {"debug_adapter"},
