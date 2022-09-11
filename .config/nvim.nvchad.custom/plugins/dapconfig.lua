ee04cf81549edf34e519996fce7f5eed0306e2bb
Bálint Böde <balint.bode@gmail.com>
2022-09-11 10:16:39 +0200

    Update dapconfig

--- .config/nvim.nvchad.custom/plugins/dapconfig.lua
+++ .config/nvim.nvchad.custom/plugins/dapconfig.lua
@@ -10,6 +10,14 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+dap.defaults.fallback.focus_terminal = true
+dap.defaults.fallback.force_external_terminal = false
+dap.defaults.fallback.terminal_win_cmd = "70vsplit new"
+
+dap.defaults.dart.focus_terminal = true
+dap.defaults.dart.force_external_terminal = false
+dap.defaults.dart.terminal_win_cmd = "70vsplit new"
+
 dap.adapters.dart = {
   type = "executable",
   command = "flutter",
