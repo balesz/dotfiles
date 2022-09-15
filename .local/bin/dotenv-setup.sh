d1fadf93bae70c0847c0aa3381c6aa9ef94e0c8d
Bálint Böde <balint.bode@gmail.com>
2022-09-15 19:39:40 +0200

    Install Oh My Zsh

--- .local/bin/dotenv-setup.sh
+++ .local/bin/dotenv-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Install Oh My Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 # Install Go
 mkdir -p ~/.local/opt ; rm -rf ~/.local/opt/go
 wget -P /tmp https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
 tar xvzf /tmp/go1.19.1.linux-amd64.tar.gz
 
-# Install Go apps
+# Install Applications
 go install github.com/anmitsu/goful@latest
 go install github.com/jesseduffield/lazygit@latest
 
