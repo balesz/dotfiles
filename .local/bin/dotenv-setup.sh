335a6b5f5a520c6394f9b27402189d55df802b1b
Balesz <balint.bode@gmail.com>
2022-09-16 10:12:13 +0200

    Change Go install script

--- .local/bin/dotenv-setup.sh
+++ .local/bin/dotenv-setup.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # Install Oh My Zsh
+cd ~
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Install Go
+GO_VERION=1.19.1
 mkdir -p ~/.local/opt ; rm -rf ~/.local/opt/go
-wget -P /tmp https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
-tar xvzf /tmp/go1.19.1.linux-amd64.tar.gz
+wget -P /tmp https://go.dev/dl/go${GO_VERION}.linux-amd64.tar.gz
+tar xvzf /tmp/go${GO_VERION}.linux-amd64.tar.gz
+rm /tmp/go${GO_VERION}.linux-amd64.tar.gz
 
 # Install Applications
 go install github.com/anmitsu/goful@latest
