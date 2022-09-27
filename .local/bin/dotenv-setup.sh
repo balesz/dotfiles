60d0970e127ddcda6383ff09bf58726eedfbebb9
Balesz <balint.bode@gmail.com>
2022-09-27 08:30:41 +0200

    Fix go unpacking

--- .local/bin/dotenv-setup.sh
+++ .local/bin/dotenv-setup.sh
@@ -8,11 +8,12 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 GO_VERION=1.19.1
 mkdir -p ~/.local/opt ; rm -rf ~/.local/opt/go
 wget -P /tmp https://go.dev/dl/go${GO_VERION}.linux-amd64.tar.gz
-tar xvzf /tmp/go${GO_VERION}.linux-amd64.tar.gz
+tar -C ~/.local/opt -xvzf /tmp/go${GO_VERION}.linux-amd64.tar.gz
 rm /tmp/go${GO_VERION}.linux-amd64.tar.gz
 
 # Install Applications
 go install github.com/anmitsu/goful@latest
 go install github.com/jesseduffield/lazygit@latest
 go install github.com/go-task/task/v3/cmd/task@latest
+go install github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
 
