52f9e55b41cb95237003f37ef9440934d56cda46
Bálint Böde <balint.bode@gmail.com>
2022-10-09 00:44:34 +0200

    Change nvim-treesitter settings

--- .config/nvim.balesz/lua/plugins/nvim-treesitter.lua
+++ .config/nvim.balesz/lua/plugins/nvim-treesitter.lua
@@ -6,12 +6,13 @@ return {
   run = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "http", "json", "lua", "make", "go", "dart", "proto" },
+      ensure_installed = { "rust", "javascript", "typescript", "tsx", "http", "json", "lua", "make", "go", "dart", "proto", "dockerfile", "java", "kotlin", "markdown", "markdown_inline", "regex", "toml", "yaml", "help" },
+      ignore_install = {},
       sync_install = true,
-      ignore_install = { "javascript" },
+      auto_install = true,
       highlight = {
         enable = true,
-        disable = { "c", "rust" },
+        disable = {},
         additional_vim_regex_highlighting = false,
       },
     }
