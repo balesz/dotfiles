c9384504714c18e181485d7de4f65f37cd45ffbb
Bálint Böde <balint.bode@gmail.com>
2022-10-09 19:25:31 +0200

    Fix filetree setup

--- .config/nvim.balesz/lua/features/filetree.lua
+++ .config/nvim.balesz/lua/features/filetree.lua
@@ -1,39 +1,37 @@
 local M = {}
 
 M.setup = function(use) 
-  use neo_tree
-end
-
---
--- https://github.com/nvim-neo-tree/neo-tree.nvim
---
-local neo_tree = {
-  'nvim-neo-tree/neo-tree.nvim',
-  requires = {
-    "nvim-lua/plenary.nvim",
-    "kyazdani42/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require('neo-tree').setup {} 
-  end
-}
-
---
--- https://github.com/nvim-tree/nvim-tree.lua
---
-local nvim_tree = {
-  "nvim-tree/nvim-tree.lua",
-  requires = "nvim-tree/nvim-web-devicons",
-  tag = "nightly",
-  config = function()
-    require("nvim-tree").setup {
-      view = {
-        side = "right",
-        preserve_window_proportions = true,
+  --
+  -- https://github.com/nvim-neo-tree/neo-tree.nvim
+  --
+  use {
+    'nvim-neo-tree/neo-tree.nvim',
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require('neo-tree').setup {} 
+    end
+  }
+  --
+  -- https://github.com/nvim-tree/nvim-tree.lua
+  --
+  use {
+    "nvim-tree/nvim-tree.lua",
+    disable = true,
+    requires = "nvim-tree/nvim-web-devicons",
+    tag = "nightly",
+    config = function()
+      require("nvim-tree").setup {
+        view = {
+          side = "right",
+          preserve_window_proportions = true,
+        }
       }
-    }
-  end
-}
+    end
+  }
+end
 
 return M
