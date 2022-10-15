4609a1711f9a697aa726fae1cdbc713d8a493d56
Bálint Böde <balint.bode@gmail.com>
2022-10-15 11:21:00 +0200

    Add catppuccin color scheme

--- .config/nvim.balesz/lua/features/appearance/colorscheme.lua
+++ .config/nvim.balesz/lua/features/appearance/colorscheme.lua
@@ -10,10 +10,23 @@ function M.setup(use)
       require('nightfox').setup {}
     end
   }
+  --
+  -- https://github.com/catppuccin/nvim
+  --
+  use {
+    'catppuccin/nvim',
+    as = 'catppuccin',
+    config = function()
+      require('catppuccin').setup {}
+    end
+  }
 end
 
 if pcall(require, 'nightfox') then
   vim.cmd 'colorscheme nordfox'
+elseif pcall(require, 'catppuccin') then
+  vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha 
+  vim.cmd "colorscheme catppuccin"
 end
 
 return M
