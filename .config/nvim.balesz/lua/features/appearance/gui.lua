0a9c38231bfb55482764227fcccdfae359f49b28
Balesz <balint.bode@gmail.com>
2022-10-18 17:18:32 +0200

    Change font to JetBrainsMono

--- .config/nvim.balesz/lua/features/appearance/gui.lua
+++ .config/nvim.balesz/lua/features/appearance/gui.lua
@@ -3,9 +3,11 @@ local M = {}
 function M.setup(use)
 end
 
-local font = "FiraCode Nerd Font:h12"
+--local font = "FiraCode Nerd Font:h11"
+local font = "JetBrainsMono Nerd Font:h11"
 if os.getenv("WSL_DISTRO_NAME") ~= "" then
-  font = "FiraCode NF:h12"
+  --font = "FiraCode NF:h11"
+  font = "JetBrainsMono NF:h11"
 end
 
 vim.opt_global.guifont = font
