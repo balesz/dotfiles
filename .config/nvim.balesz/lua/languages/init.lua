214c45e03369bd35bbc4b5b52183db250d0b8d47
Bálint Böde <balint.bode@gmail.com>
2022-10-20 22:04:53 +0200

    Adding some lsp keymap

--- .config/nvim.balesz/lua/languages/init.lua
+++ .config/nvim.balesz/lua/languages/init.lua
@@ -20,9 +20,17 @@ function M.setup(use)
   }
 end
 
-vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, { desc = "Lsp - Format" })
-vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover, { desc = "Lsp - Hover" })
-
 require 'languages/lua'
+
+vim.api.nvim_create_augroup("BaleszLsp", { clear = true })
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "BaleszLsp",
+  callback = function(args)
+    vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, { buffer = args.buf, desc = "Code Action" })
+    vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, { buffer = args.buf, desc = "Format" })
+    vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover, { buffer = args.buf, desc = "Hover" })
+    vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.references, { buffer = args.buf, desc = "References" })
+  end
+})
 
 return M
