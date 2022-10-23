9536eda20b40c7d6e196464d84accf96b3aea943
Bálint Böde <balint.bode@gmail.com>
2022-10-23 11:20:42 +0200

    Refactroing terminal config

--- .config/nvim.balesz/lua/features/terminal.lua
+++ .config/nvim.balesz/lua/features/terminal.lua
@@ -1,29 +1,30 @@
 local M = {}
 
 function M.setup(use)
-  --
-  -- https://github.com/akinsho/toggleterm.nvim
-  --
   use {
     "akinsho/toggleterm.nvim",
-    tag = '*',
+    tag = "*",
     config = function()
       require("toggleterm").setup {}
     end
   }
+  use {
+    "voldikss/vim-floaterm",
+    disable = true,
+  }
 end
 
-vim.api.nvim_create_augroup("BaleszTerminal", {clear=true})
-vim.api.nvim_create_autocmd({"TermOpen"}, {
+vim.api.nvim_create_augroup("BaleszTerminal", {})
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
   pattern = "*",
   group = "BaleszTerminal",
   callback = function()
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
-    vim.cmd 'startinsert'
+    vim.cmd "startinsert"
   end
 })
-vim.api.nvim_create_autocmd({"TermClose"}, {
+vim.api.nvim_create_autocmd({ "TermClose" }, {
   pattern = "*",
   group = "BaleszTerminal",
   callback = function()
@@ -33,9 +34,13 @@ vim.api.nvim_create_autocmd({"TermClose"}, {
   end
 })
 
-vim.keymap.set("", "<Leader>ti", "<Cmd>ToggleTerm direction=float<CR>", {noremap=true})
-vim.keymap.set("", "<Leader>th", "<Cmd>ToggleTerm size=16 direction=horizontal<CR>", {noremap=true})
-vim.keymap.set("", "<Leader>tv", "<Cmd>ToggleTerm size=80 direction=vertical<CR>", {noremap=true})
-vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", {noremap=true})
+local floating = "<Cmd>ToggleTerm direction=float<CR>"
+local horizontal = "<Cmd>ToggleTerm size=16 direction=horizontal<CR>"
+local vertical = "<Cmd>ToggleTerm size=80 direction=vertical<CR>"
+
+vim.keymap.set("", "<Leader>ti", floating)
+vim.keymap.set("", "<Leader>th", horizontal)
+vim.keymap.set("", "<Leader>tv", vertical)
+vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
 
 return M
