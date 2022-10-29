fb8e79c7dfad1b57b3d7fc5f7e747bb3a1803728
Bálint Böde <balint.bode@gmail.com>
2022-10-29 19:18:30 +0200

    Add keymap to opening lazygit in floating window

--- .config/nvim.balesz/lua/features/git.lua
+++ .config/nvim.balesz/lua/features/git.lua
@@ -12,4 +12,18 @@ function M.setup(use)
   }
 end
 
+local ok, toggleterm = pcall(require, "toggleterm.terminal")
+if not ok then return end
+
+local lazygit = toggleterm.Terminal:new {
+  cmd = "lazygit",
+  direction = "float",
+  close_on_exit = true,
+  hidden = true,
+}
+
+vim.keymap.set("", "<Leader>gl", function()
+  lazygit:toggle()
+end, { desc = "Open layzgit" })
+
 return M
