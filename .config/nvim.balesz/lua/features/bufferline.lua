b26f091e1aef68a91504c62469533d8202d6637b
Bálint Böde <balint.bode@gmail.com>
2022-10-31 09:57:42 +0100

    Change buffer delete command

--- .config/nvim.balesz/lua/features/bufferline.lua
+++ .config/nvim.balesz/lua/features/bufferline.lua
@@ -7,7 +7,7 @@ function M.setup(use)
   use {
     "romgrk/barbar.nvim",
     requires = "kyazdani42/nvim-web-devicons",
-    disable = false,
+    disable = true,
     config = function()
       require("bufferline").setup {
         animation = true,
@@ -24,28 +24,39 @@ function M.setup(use)
       vim.keymap.set("n", "<S-TAB>", "<cmd>BufferPrevious<CR>")
       vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferMoveNext<CR>")
       vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferMovePrevious<CR>")
-      vim.keymap.set("n", "x", "<Cmd>BufferClose<CR>")
-      vim.api.nvim_create_user_command("BaleszBuffDelete", "BufferClose", {})
+      vim.api.nvim_create_user_command("BaleszBuffDelete", function(params)
+        vim.cmd("BufferClose " .. params.args)
+      end, { nargs = "?" })
     end
   }
   use {
     "akinsho/bufferline.nvim",
     requires = "kyazdani42/nvim-web-devicons",
     tag = "v3.*",
-    disable = true,
+    disable = false,
     config = function()
-      require("bufferline").setup {}
+      local close_command = "BaleszBuffDelete %d"
+      require("bufferline").setup {
+        options = {
+          close_command = close_command,
+          right_mouse_command = nil,
+          middle_mouse_command = close_command,
+        }
+      }
     end,
     setup = function()
       vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
       vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
       vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferLineMoveNext<CR>")
       vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferLineMovePrev<CR>")
-      vim.keymap.set("n", "x", "<Cmd>Bdelete<CR>")
-      vim.api.nvim_create_user_command("BaleszBuffDelete", "Bdelete", {})
+      vim.api.nvim_create_user_command("BaleszBuffDelete", function(params)
+        vim.cmd("Bdelete " .. params.args)
+      end, { nargs = "?" })
     end
   }
 end
+
+vim.keymap.set("n", "x", "<Cmd>BaleszBuffDelete<CR>")
 
 function M.close()
   vim.cmd "BaleszBuffDelete"
