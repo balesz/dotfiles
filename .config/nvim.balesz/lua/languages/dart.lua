d9dc34e09700b2bfeab7eb0da62285d12e0f9f9f
Bálint Böde <balint.bode@gmail.com>
2022-11-04 23:51:16 +0100

    Refactoring languages/dart

--- .config/nvim.balesz/lua/languages/dart.lua
+++ .config/nvim.balesz/lua/languages/dart.lua
@@ -1,8 +1,44 @@
 local M = {}
 
 local ok_lsp, lspconfig = pcall(require, "lspconfig")
-local ok_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-local ok_flutter, flutter = pcall(require, "utils/flutter")
+local ok_cmp_nvim_lsp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+
+local function get_capabilities()
+  return ok_cmp_nvim_lsp and cmp_nvim_lsp.default_capabilities() or nil
+end
+
+local function get_root_dir(filename, bufnr)
+  local clients = vim.lsp.get_active_clients({ name = "dartls" })
+  if #clients > 0 then return clients[1].config.root_dir end
+  return lspconfig.util.root_pattern("pubspec.yaml")(filename, bufnr)
+end
+
+local function get_excluded_folders()
+  local sdk_dirs = {}
+
+  if vim.fn.executable("fvm") then
+    local list = vim.fn.system("fvm list"):gsub("Cache Directory:  ", "")
+    for line in vim.gsplit(list, "\n") do
+      if line == "" then
+      elseif line:match("/[%p%w]-/[%p%w]+") then
+        table.insert(sdk_dirs, line)
+      elseif line:match("%d+%.%d+%.%d+") then
+        table.insert(sdk_dirs, sdk_dirs[1] .. "/" .. line)
+      end
+    end
+  end
+
+  local flutter_path = vim.fn.resolve(vim.fn.exepath("flutter"))
+  sdk_dirs[1] = flutter_path:gsub("/bin/flutter", "")
+
+  local result = { vim.env.HOME .. "/.pub-cache" }
+  for _, it in pairs(sdk_dirs) do
+    table.insert(result, it .. "/.pub-cache")
+    table.insert(result, it .. "/packages")
+  end
+
+  return result
+end
 
 function M.setup(_)
   --
@@ -10,14 +46,8 @@ function M.setup(_)
   -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md
   --
   local _ = ok_lsp and lspconfig.dartls.setup {
-    root_dir = function(filename, bufnr)
-      local clients = vim.lsp.get_active_clients({ name = "dartls" })
-      if #clients > 0 then return clients[1].config.root_dir end
-      return lspconfig.util.root_pattern("pubspec.yaml")(filename, bufnr)
-    end,
-    capabilities = (function()
-      return ok_cmp and cmp_nvim_lsp.default_capabilities() or nil
-    end)(),
+    root_dir = get_root_dir,
+    capabilities = get_capabilities(),
     init_options = {
       onlyAnalyzeProjectsWithOpenFiles = true,
       suggestFromUnimportedLibraries = true,
@@ -31,9 +61,7 @@ function M.setup(_)
         enableSdkFormatter = true,
         lineLength = 120,
         showTodos = true,
-        analysisExcludedFolders = ok_flutter
-            and flutter.get_excluded_folders()
-            or {},
+        analysisExcludedFolders = get_excluded_folders(),
       }
     }
   }
