89aa35ead6055178285cd1f169ab408d396b0bec
Bálint Böde <balint.bode@gmail.com>
2022-11-05 15:04:20 +0100

    Using formatting_sync on save

--- .config/nvim.balesz/lua/languages/init.lua
+++ .config/nvim.balesz/lua/languages/init.lua
@@ -45,7 +45,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(args)
     local client = vim.lsp.get_active_clients({ bufnr = args.buf })
     if #client == 0 then return end
-    vim.lsp.buf.format({ bufnr = args.buf })
+    vim.lsp.buf.formatting_sync({ bufnr = args.buf })
   end
 })
 
