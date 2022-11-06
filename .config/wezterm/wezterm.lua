03b60378d9874fa16ad9752c241563b1c0fdf156
Bálint Böde <balint.bode@gmail.com>
2022-11-06 17:59:36 +0100

    Format wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,34 +1,33 @@
-local wezterm = require 'wezterm';
+local wezterm = require "wezterm";
 
 local default_prog = nil
-local font = 'JetBrainsMono Nerd Font'
+local font = "JetBrainsMono Nerd Font"
 
-if os.getenv('OS') == 'Windows_NT' then
-  default_prog = { 'wsl', '-d', 'Ubuntu', '--cd', '~' }
-  font = 'JetBrainsMono NF'
+if os.getenv("OS") == "Windows_NT" then
+  default_prog = { "wsl", "-d", "Ubuntu", "--cd", "~" }
+  font = "JetBrainsMono NF"
 end
 
 return {
-  color_scheme = 'Operator Mono Dark',
+  color_scheme = "Operator Mono Dark",
   default_prog = default_prog,
-  exit_behavior= 'Close',
+  exit_behavior = "Close",
   font = wezterm.font(font),
   font_size = 11,
   initial_cols = 120,
   initial_rows = 40,
-  use_fancy_tab_bar= true,
+  use_fancy_tab_bar = true,
   tab_bar_at_bottom = true,
   window_background_opacity = 0.9,
   window_padding = {
-    left = '0cell',
-    right = '0cell',
-    top = '0cell',
-    bottom = '0cell',
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
   },
   launch_menu = {
-    { args = { 'powershell' } },
-    { args = { 'top' } },
-    { args = { 'lazygit' } },
+    { args = { "powershell" } },
+    { args = { "top" } },
+    { args = { "lazygit" } },
   },
 }
-
