dbc03606937adda4012120dc27a2545806c61cda
Bálint Böde <balint.bode@gmail.com>
2022-11-12 15:18:18 +0100

    Configure auto import and indent for Golang

---
+++ .config/nvim.balesz/ftplugin/go.lua
@@ -0,0 +1,6 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = false
--- .config/nvim.balesz/lua/languages/go.lua
+++ .config/nvim.balesz/lua/languages/go.lua
@@ -11,7 +11,29 @@ function M.setup(_)
   --
   -- https://github.com/golang/tools/tree/master/gopls
   --
-  local _ = ok_lsp and lspconfig.gopls.setup {}
+  local _ = ok_lsp and lspconfig.gopls.setup {
+    capabilities = get_capabilities(),
+  }
 end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = "BaleszLsp",
+  pattern = "*.go",
+  callback = function(args)
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(
+      args.bufnr, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding
+              or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+  end
+})
 
 return M
