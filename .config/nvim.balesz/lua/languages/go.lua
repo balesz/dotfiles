e3a7477bb4e5e045b3b271cec4452d991e570477
Bálint Böde <balint.bode@gmail.com>
2022-11-15 22:49:21 +0100

    Refactor lsp autocommands

--- .config/nvim.balesz/lua/languages/go.lua
+++ .config/nvim.balesz/lua/languages/go.lua
@@ -16,24 +16,4 @@ function M.setup(_)
   }
 end
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = "BaleszLsp",
-  pattern = "*.go",
-  callback = function(args)
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(
-      args.bufnr, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding
-              or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-  end
-})
-
 return M
--- .config/nvim.balesz/lua/languages/init.lua
+++ .config/nvim.balesz/lua/languages/init.lua
@@ -23,39 +23,67 @@ function M.setup(use)
   require("languages/lua").setup(use)
 end
 
-vim.api.nvim_create_augroup("BaleszLsp", {})
+local function set_keymaps(buf)
+  vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action,
+    { buffer = buf, desc = "Code Action" })
+  vim.keymap.set("n", "<Leader>ld", vim.lsp.buf.references,
+    { buffer = buf, desc = "References" })
+  vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format,
+    { buffer = buf, desc = "Format" })
+  vim.keymap.set("n", "<Leader>lg", vim.lsp.buf.definition,
+    { buffer = buf, desc = "Definition" })
+  vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover,
+    { buffer = buf, desc = "Hover" })
+  vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename,
+    { buffer = buf, desc = "Rename" })
+end
 
+local function format_on_save(buf)
+  local client = vim.lsp.get_active_clients({ bufnr = buf })
+  if #client == 0 then return end
+  vim.lsp.buf.format({ async = false, bufnr = buf })
+end
+
+local function organize_imports(buf)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(buf, "textDocument/codeAction", params)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding
+            or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+      end
+    end
+  end
+end
+
+local function show_diagnostic(buf)
+  vim.diagnostic.show(nil, buf)
+end
+
+--
+-- Autocommands
+--
+vim.api.nvim_create_augroup("BaleszLsp", {})
 vim.api.nvim_create_autocmd("LspAttach", {
   group = "BaleszLsp",
   callback = function(args)
-    vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action,
-      { buffer = args.buf, desc = "Code Action" })
-    vim.keymap.set("n", "<Leader>ld", vim.lsp.buf.references,
-      { buffer = args.buf, desc = "References" })
-    vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format,
-      { buffer = args.buf, desc = "Format" })
-    vim.keymap.set("n", "<Leader>lg", vim.lsp.buf.definition,
-      { buffer = args.buf, desc = "Definition" })
-    vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover,
-      { buffer = args.buf, desc = "Hover" })
-    vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename,
-      { buffer = args.buf, desc = "Rename" })
+    set_keymaps(args.buf)
   end
 })
-
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = "BaleszLsp",
   callback = function(args)
-    local client = vim.lsp.get_active_clients({ bufnr = args.buf })
-    if #client == 0 then return end
-    vim.lsp.buf.format({ async = false, bufnr = args.buf })
+    organize_imports(args.buf)
+    format_on_save(args.buf)
   end
 })
-
 vim.api.nvim_create_autocmd("BufWritePost", {
   group = "BaleszLsp",
   callback = function(args)
-    vim.diagnostic.show(nil, args.buf)
+    show_diagnostic(args.buf)
   end
 })
 
