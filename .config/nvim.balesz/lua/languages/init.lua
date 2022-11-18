f5c36c4527188ac5d856174d31da369e1f5c4ce2
Bálint Böde <balint.bode@gmail.com>
2022-11-18 21:30:16 +0100

    Fix organize import autocmd

--- .config/nvim.balesz/lua/languages/init.lua
+++ .config/nvim.balesz/lua/languages/init.lua
@@ -47,16 +47,21 @@ end
 local function organize_imports(buf)
   local client = vim.lsp.get_active_clients({ bufnr = buf })
   if #client == 0 then return end
+
+  local method = "textDocument/codeAction"
+  local kind = "source.organizeImports"
+
   local params = vim.lsp.util.make_range_params()
-  params.context = { only = { "source.organizeImports" }, diagnostics = {} }
-  local result = vim.lsp.buf_request_sync(buf, "textDocument/codeAction", params)
+  params.context = { only = { kind }, diagnostics = {} }
+  local result = vim.lsp.buf_request_sync(buf, method, params)
+
   for cid, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
-      if r.edit then
-        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding
-            or "utf-16"
+      if r.kind == kind and r.edit then
+        local clnt = vim.lsp.get_client_by_id(cid) or {}
+        local enc = clnt.offset_encoding or "utf-16"
         vim.lsp.util.apply_workspace_edit(r.edit, enc)
-      elseif r.command then
+      elseif r.kind == kind and r.command then
         vim.lsp.buf.execute_command(r.command)
       end
     end
