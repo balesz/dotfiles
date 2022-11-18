fd63e056968730f48cd6ba932680f7be51fb1e0c
Bálint Böde <balint.bode@gmail.com>
2022-11-18 18:07:56 +0100

    Change editor config

--- .config/nvim.balesz/lua/features/editor.lua
+++ .config/nvim.balesz/lua/features/editor.lua
@@ -5,27 +5,27 @@ function M.setup(use)
   -- https://github.com/lukas-reineke/indent-blankline.nvim
   --
   use {
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     event = "BufRead",
     config = function()
-      require('indent_blankline').setup {
-        char = '▏',
+      require("indent_blankline").setup {
+        char_blankline = "|",
         indentLine_enabled = 1,
         show_current_context = true,
         show_current_context_start = true,
-        show_end_of_line = false,
+        show_end_of_line = true,
         show_trailing_blankline_indent = false,
         space_char_blankline = " ",
-     }
+      }
     end
   }
   --
   -- https://github.com/karb94/neoscroll.nvim
   --
   use {
-    'karb94/neoscroll.nvim',
+    "karb94/neoscroll.nvim",
     config = function()
-      require('neoscroll').setup {}
+      require("neoscroll").setup {}
     end
   }
 end
@@ -35,8 +35,8 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 vim.opt.clipboard = "unnamedplus"
-vim.opt.tabstop=2
-vim.opt.shiftwidth=2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.smarttab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
