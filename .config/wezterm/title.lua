e5b0a7c5b708faf921031425464906c3a4c218e3
Bálint Böde <balint.bode@gmail.com>
2022-11-22 21:45:12 +0100

    Remove title logic from wezterm config

--- .config/wezterm/title.lua
+++ .config/wezterm/title.lua
@@ -1,23 +1,5 @@
 local wezterm = require "wezterm";
 
-local function get_icon(pane)
-  if pane.foreground_process_name:find("nvim") then
-    return "  | "
-  elseif pane.foreground_process_name:find("ssh") then
-    return "  | "
-  end
-  return " "
-end
-
-local function get_title(pane)
-  local title = pane.foreground_process_name:gsub("/.*/(.*)", "%1")
-  if title == "nvim" then
-    local dir = pane.current_working_dir
-    title = dir:gsub("file://.*/(.+)", "%1")
-  end
-  return title
-end
-
 wezterm.on(
   "format-tab-title",
   function(tab)
@@ -25,9 +7,7 @@ wezterm.on(
     if tab.is_active then
       table.insert(result, { Background = { Color = "brown" } })
     end
-    local icon = get_icon(tab.active_pane)
-    local title = get_title(tab.active_pane)
-    table.insert(result, { Text = icon .. title })
+    table.insert(result, { Text = tab.active_pane.title })
     return result
   end
 )
@@ -35,7 +15,7 @@ wezterm.on(
 wezterm.on(
   "format-window-title",
   function(tab)
-    local title = get_title(tab.active_pane)
+    local title = tab.active_pane.title
     return title
   end
 )
