c7f5d6cdbf60bdf663c4eeec5c12a5517c93f06e
Bálint Böde <balint.bode@gmail.com>
2022-11-22 22:13:19 +0100

    Add autocmd for title change

--- .config/nvim.balesz/lua/features/appearance/gui.lua
+++ .config/nvim.balesz/lua/features/appearance/gui.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.setup(use)
+function M.setup(_)
 end
 
 local font = ""
@@ -10,8 +10,21 @@ if os.getenv("WSL_DISTRO_NAME") ~= "" then
   font = font:gsub("Nerd Font", "NF")
 end
 
+vim.opt.title = true
 vim.opt.guifont = font .. ":h11"
 vim.opt.guicursor = "n-v-c-sm:block-lCursor,i-ci-ve:ver25-lCursor,r-cr-o:hor25-lCursor"
 vim.opt.termguicolors = true
+
+vim.api.nvim_create_augroup("BaleszGui", {})
+
+vim.api.nvim_create_autocmd({ "DirChanged" }, {
+  pattern = "global",
+  group = "BaleszGui",
+  callback = function()
+    local title = vim.fn.getcwd()
+    title = title:gsub("/.*/(.*)", "%1")
+    vim.opt.titlestring = title
+  end
+})
 
 return M
