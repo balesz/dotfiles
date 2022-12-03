5aa58b225f433dee66170690c51de7be48e540c8
Bálint Böde <balint.bode@gmail.com>
2022-12-03 17:00:54 +0100

    Add noice.nvim plugin

--- .config/nvim.balesz/lua/features/appearance/gui.lua
+++ .config/nvim.balesz/lua/features/appearance/gui.lua
@@ -5,6 +5,27 @@ local ok_notify, notify = pcall(require, "notify")
 function M.setup(use)
   use "MunifTanjim/nui.nvim"
   use "rcarriga/nvim-notify"
+  use {
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup {
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      }
+    end
+  }
   vim.notify = ok_notify and notify or vim.notify
 end
 
