b249cc6e9320181056ac7bf76c87d2052f403413
Bálint Böde <balint.bode@gmail.com>
2022-12-04 22:54:51 +0100

    Add os.capture function to utils

--- .config/nvim.balesz/lua/utils/init.lua
+++ .config/nvim.balesz/lua/utils/init.lua
@@ -6,3 +6,14 @@ function LoadCurrent()
   local filename = vim.api.nvim_buf_get_name(0)
   vim.cmd("luafile" .. filename)
 end
+
+function os.capture(cmd)
+  local out = assert(io.popen(cmd, "r"))
+  local str = assert(out:read("*a"))
+  out:close()
+  local result = {}
+  for s in str:gmatch("[^\r\n]+") do
+    table.insert(result, s)
+  end
+  return result
+end
