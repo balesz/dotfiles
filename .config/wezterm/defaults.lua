1ed15cad7af3f9c3cef8b0f0e2d03a539d462cb2
Balesz <balint.bode@gmail.com>
2022-12-05 18:53:49 +0100

    Set wsl domain correctly

--- .config/wezterm/defaults.lua
+++ .config/wezterm/defaults.lua
@@ -4,8 +4,13 @@ local M = { cwd = "~", domain = "local", prog = { "zsh" } }
 
 if wezterm.config_dir:find("wsl.localhost") then
   local distrib = wezterm.config_dir:gsub("\\\\wsl.localhost\\(%a*)\\.*", "%1")
-  M.prog = { "wsl", "-d", distrib, "--cd", "~" }
-  --M.domain = "WSL:" .. distrib
+  for _, dom in ipairs(wezterm.default_wsl_domains()) do
+    if dom.name == "WSL:" .. distrib then
+      dom.default_cwd = M.cwd
+      dom.default_prog = M.prog
+      M.domain = dom.name
+    end
+  end
 end
 
 return M
