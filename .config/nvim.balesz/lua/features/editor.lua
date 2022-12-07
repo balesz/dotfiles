6531095ce423993979733abcbe2763705742ef35
Bálint Böde <balint.bode@gmail.com>
2022-12-07 20:38:45 +0100

    Change feature/editor

--- .config/nvim.balesz/lua/features/editor.lua
+++ .config/nvim.balesz/lua/features/editor.lua
@@ -32,6 +32,7 @@ end
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "tab: ⋅"
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 2
