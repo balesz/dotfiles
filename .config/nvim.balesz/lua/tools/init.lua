5d87ce3c388267ab35f54bfbf8a7a2837877c4fd
Bálint Böde <balint.bode@gmail.com>
2022-12-10 18:40:06 +0100

    Clean tools/init.lua

--- .config/nvim.balesz/lua/tools/init.lua
+++ .config/nvim.balesz/lua/tools/init.lua
@@ -8,27 +8,11 @@ end
 return M
 
 --
---
--- ## Common
--- https://github.com/MunifTanjim/nui.nvim
---
 -- ## User Interface
 -- https://github.com/feline-nvim/feline.nvim
 -- https://github.com/sidebar-nvim/sidebar.nvim
 -- https://github.com/ghillb/cybu.nvim
--- https://github.com/sindrets/winshift.nvim
--- https://github.com/petertriho/nvim-scrollbar
---
--- ## Editor
--- https://github.com/mg979/vim-visual-multi
--- https://github.com/azabiong/vim-highlighter
 --
 -- ## LSP
--- https://github.com/neoclide/coc.nvim
 -- https://github.com/jose-elias-alvarez/null-ls.nvim
--- https://github.com/jamespwilliams/neovim-go-nix-develop
 -- https://github.com/folke/trouble.nvim
---
--- ## Tools
--- https://github.com/akinsho/flutter-tools.nvim
---
