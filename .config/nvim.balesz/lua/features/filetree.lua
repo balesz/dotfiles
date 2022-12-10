af488c85fda0b26bcaa91bcad3ffeafc2e200828
Bálint Böde <balint.bode@gmail.com>
2022-12-10 22:56:17 +0100

    Remove nvim-tree.lua

--- .config/nvim.balesz/lua/features/filetree.lua
+++ .config/nvim.balesz/lua/features/filetree.lua
@@ -32,55 +32,10 @@ function M.setup(use)
       }
     end
   }
-  use {
-    "nvim-tree/nvim-tree.lua",
-    disable = true,
-    requires = "nvim-tree/nvim-web-devicons",
-    tag = "nightly",
-    config = function()
-      require("nvim-tree").setup {
-        sync_root_with_cwd = true,
-        reload_on_bufenter = true,
-        view = {
-          side = "left",
-          width = 40,
-          adaptive_size = false,
-          centralize_selection = true,
-          preserve_window_proportions = true,
-          signcolumn = "auto",
-          mappings = {
-            list = {
-              { key = ".", action = "cd" },
-              { key = "<A-r>", action = "run_file_command" },
-              { key = "?", action = "toggle_help" },
-            },
-          },
-        },
-        renderer = {
-          group_empty = true
-        },
-        update_focused_file = {
-          enable = true,
-          update_root = false,
-        },
-        filters = {
-          dotfiles = false,
-          custom = { "\\.git" },
-        },
-        diagnostics = {
-          enable = true,
-        },
-      }
-    end
-  }
 end
 
 vim.keymap.set("", "<Leader>ft", function()
-  if pcall(require, "neo-tree") then
-    vim.cmd "NeoTreeFocusToggle"
-  elseif pcall(require, "nvim-tree") then
-    vim.cmd "NvimTreeFocus"
-  end
+  vim.cmd "NeoTreeFocusToggle"
 end, { desc = "Open Neo-tree" })
 
 if ok_terminal then
