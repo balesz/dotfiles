329fdbec498126ddd063e518b4802f267b612ce6
Bálint Böde <balint.bode@gmail.com>
2022-12-10 23:23:31 +0100

    Remove barbar.nvim and change bufferline's config

--- .config/nvim.balesz/lua/features/buffer.lua
+++ .config/nvim.balesz/lua/features/buffer.lua
@@ -5,35 +5,9 @@ function M.setup(use)
     "famiu/bufdelete.nvim",
   }
   use {
-    "romgrk/barbar.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    disable = true,
-    config = function()
-      require("bufferline").setup {
-        animation = true,
-        auto_hide = false,
-        tabpages = true,
-        closable = true,
-        clickable = true,
-        icons = true,
-        no_name_title = "empty",
-      }
-    end,
-    setup = function()
-      vim.keymap.set("n", "<TAB>", "<cmd>BufferNext<CR>")
-      vim.keymap.set("n", "<S-TAB>", "<cmd>BufferPrevious<CR>")
-      vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferMoveNext<CR>")
-      vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferMovePrevious<CR>")
-      vim.api.nvim_create_user_command("BaleszBuffDelete", function(params)
-        vim.cmd("BufferClose " .. params.args)
-      end, { nargs = "?" })
-    end
-  }
-  use {
     "akinsho/bufferline.nvim",
     tag = "v3.*",
     requires = "kyazdani42/nvim-web-devicons",
-    disable = true,
     config = function()
       local close_command = "BaleszBuffDelete %d"
       require("bufferline").setup {
@@ -41,6 +15,15 @@ function M.setup(use)
           close_command = close_command,
           right_mouse_command = close_command,
           middle_mouse_command = close_command,
+          separator_style = "slant",
+          offsets = {
+            {
+              filetype = "neo-tree",
+              text = "File Explorer",
+              text_align = "center",
+              separator = true,
+            },
+          },
         }
       }
     end,
@@ -49,9 +32,6 @@ function M.setup(use)
       vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
       vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferLineMoveNext<CR>")
       vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferLineMovePrev<CR>")
-      vim.api.nvim_create_user_command("BaleszBuffDelete", function(params)
-        vim.cmd("Bdelete " .. params.args)
-      end, { nargs = "?" })
     end
   }
   use {
@@ -67,19 +47,12 @@ function M.setup(use)
       --vim.keymap.set("n", "J", "<Plug>(CybuNext)")
       vim.keymap.set({ "n", "v" }, "<Tab>", "<Plug>(CybuLastusedNext)")
       vim.keymap.set({ "n", "v" }, "<S-Tab>", "<Plug>(CybuLastusedPrev)")
-      vim.api.nvim_create_user_command("BaleszBuffDelete", function(params)
-        vim.cmd("Bdelete " .. params.args)
-      end, { nargs = "?" })
     end
   }
 end
 
-function M.close()
-  vim.cmd "BaleszBuffDelete"
-end
+function M.close() vim.cmd "Bdelete" end
 
-vim.keymap.set("n", "x", function()
-  M.close()
-end)
+vim.keymap.set("n", "x", M.close)
 
 return M
