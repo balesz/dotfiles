8269eecc5aa9b367a3b6b821c7ce34cc34535284
Bálint Böde <balint.bode@gmail.com>
2023-02-26 10:05:17 +0100

    Refactor features/buffer

--- .config/nvim.balesz/lua/features/buffer/setup.lua
+++ .config/nvim.balesz/lua/features/buffer/setup.lua
@@ -1,34 +1,38 @@
 local close_command = "Bdelete! %d"
 
-require("bufferline").setup {
-  options = {
-    always_show_bufferline = true,
-    diagnostics = "nvim_lsp",
-    diagnostics_update_in_insert = true,
-    close_command = close_command,
-    right_mouse_command = close_command,
-    middle_mouse_command = close_command,
-    separator_style = "slant",
-    sort_by = "insert_after_current",
-    offsets = {
-      {
-        filetype = "neo-tree",
-        text = "File Explorer",
-        text_align = "center",
-        separator = true,
-      },
-    },
-  }
-}
-
-require("cybu").setup {}
-
 vim.keymap.set("n", "x", "<cmd>Bdelete<CR>")
-vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferLineMoveNext<CR>")
-vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferLineMovePrev<CR>")
---vim.keymap.set("n", "K", "<Plug>(CybuPrev)")
---vim.keymap.set("n", "J", "<Plug>(CybuNext)")
---vim.keymap.set({ "n", "v" }, "<Tab>", "<Plug>(CybuLastusedNext)")
---vim.keymap.set({ "n", "v" }, "<S-Tab>", "<Plug>(CybuLastusedPrev)")
+
+if pcall(require, "bufferline") then
+  require("bufferline").setup {
+    options = {
+      always_show_bufferline = true,
+      diagnostics = "nvim_lsp",
+      diagnostics_update_in_insert = true,
+      close_command = close_command,
+      right_mouse_command = close_command,
+      middle_mouse_command = close_command,
+      separator_style = "slant",
+      sort_by = "insert_after_current",
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "File Explorer",
+          text_align = "center",
+          separator = true,
+        },
+      },
+    }
+  }
+  vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
+  vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
+  vim.keymap.set("n", "<Leader>t<TAB>", "<cmd>BufferLineMoveNext<CR>")
+  vim.keymap.set("n", "<Leader>t<S-TAB>", "<cmd>BufferLineMovePrev<CR>")
+end
+
+if pcall(require, "cybu") then
+  require("cybu").setup {}
+  --vim.keymap.set("n", "K", "<Plug>(CybuPrev)")
+  --vim.keymap.set("n", "J", "<Plug>(CybuNext)")
+  --vim.keymap.set({ "n", "v" }, "<Tab>", "<Plug>(CybuLastusedNext)")
+  --vim.keymap.set({ "n", "v" }, "<S-Tab>", "<Plug>(CybuLastusedPrev)")
+end
