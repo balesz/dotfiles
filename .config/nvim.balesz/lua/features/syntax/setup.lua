304af9f7ff1d9082d9c1d8114bc9308f5911e2cf
Bálint Böde <balint.bode@gmail.com>
2023-02-28 17:58:25 +0100

    Add bash to treesitter

--- .config/nvim.balesz/lua/features/syntax/setup.lua
+++ .config/nvim.balesz/lua/features/syntax/setup.lua
@@ -1,8 +1,8 @@
 if pcall(require, "nvim-treesitter") then
   require "nvim-treesitter.configs".setup {
     ensure_installed = { "http", "json", "lua", "make", "go", "dart", "proto",
-      "dockerfile", "markdown", "markdown_inline", "regex", "toml", "yaml",
-      "help" },
+      "bash", "dockerfile", "markdown", "markdown_inline", "regex",
+      "toml", "yaml", "help" },
     ignore_install = {},
     sync_install = true,
     auto_install = true,
