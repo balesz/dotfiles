8dec741bebe56b437916bb0ed10ec41c1d693270
Bálint Böde <balint.bode@gmail.com>
2023-03-12 23:42:03 +0100

    Disable noice

--- .config/nvim.balesz/lua/features/ui/init.lua
+++ .config/nvim.balesz/lua/features/ui/init.lua
@@ -2,7 +2,10 @@ return {
   "MunifTanjim/nui.nvim",
   "rcarriga/nvim-notify",
   "stevearc/dressing.nvim",
-  "folke/noice.nvim",
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" }
