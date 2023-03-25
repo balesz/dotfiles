af66529cf88aa2f8577206fcee22223a1f11edd1
Bálint Böde <balint.bode@gmail.com>
2023-03-25 13:41:29 +0100

    Update xplr version

--- .local/bin/dotenv.sh
+++ .local/bin/dotenv.sh
@@ -99,7 +99,7 @@ install_apps () {
   DIR=`mktemp -d`
   wget -P $DIR https://github.com/zellij-org/zellij/releases/download/v0.35.2/zellij-x86_64-unknown-linux-musl.tar.gz
   sudo tar -C /usr/local/bin -xvzf $DIR/zellij-x86_64-unknown-linux-musl.tar.gz
-  wget -P $DIR https://github.com/sayanarijit/xplr/releases/download/v0.20.2/xplr-linux.tar.gz
+  wget -P $DIR https://github.com/sayanarijit/xplr/releases/download/v0.21.1/xplr-linux.tar.gz
   sudo tar -C /usr/local/bin -xvzf $DIR/xplr-linux.tar.gz
   rm -rf $DIR
 }
