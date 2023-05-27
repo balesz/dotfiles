430767f96f13ad23c7e13303c7b18de66d3528bf
Bálint Böde <balint.bode@gmail.com>
2023-05-27 11:36:12 +0200

    Add nap and gum to goapps in dotenv.sh

--- .local/bin/dotenv.sh
+++ .local/bin/dotenv.sh
@@ -143,6 +143,8 @@ install_goapps () {
   go install github.com/go-task/task/v3/cmd/task@latest
   go install github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
   go install github.com/xo/usql@latest
+  go install github.com/maaslalani/nap@main
+  go install github.com/charmbracelet/gum@latest
 }
 
 install_protoc () {
