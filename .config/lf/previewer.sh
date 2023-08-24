bf8a1fa1479cdf54f17ce86afea596eb1da1449f
Balesz <balint.bode@gmail.com>
2023-08-24 21:03:41 +0200

    Change lf previewer

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-CMD="cat"
+CAT="cat"
 test -e `which highlight` && CMD="highlight -O ansi"
 test -e `which pygmentize` && CMD="pygmentize -g"
 
-case "$1" in
-  *.tar*) tar tf "$1";;
-  *.tgz) tar tf "$1";;
-  *.zip) unzip -l "$1";;
-  *.rar) unrar l "$1";;
-  *.7z) 7z l "$1";;
-  *.pdf) pdftotext "$1" -;;
-  *) $CMD "$1";;
+TYPE=`file --mime-type -Lb "$1"`
+
+case "$TYPE" in
+  application/x-tar) tar tf "$1";;
+  application/zip) unzip -l "$1";;
+  application/json) $CMD "$1";;
+  text/*) $CMD "$1";;
+  *) echo $TYPE
 esac
