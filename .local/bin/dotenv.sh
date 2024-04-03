22255ed655516fb23e298b80120de84b106b8cba
Bálint Böde <balint.bode@gmail.com>
2024-04-03 18:10:18 +0200

    Update helix version

--- .local/bin/dotenv.sh
+++ .local/bin/dotenv.sh
@@ -155,7 +155,7 @@ install_apps () {
   DIR=`mktemp -d`
   VER_BAT=0.23.0
   VER_GIT_DELTA=0.16.5
-  VER_HELIX=23.10
+  VER_HELIX=24.03
   VER_XPLR=0.21.2
   VER_ZELLIJ=0.39.2
   if [ `uname -o` = Android ]; then
